0e1f6b48346fe9a4ea9f9baa91c092989cdb0cc2
shubro007 <136472052+shubro007@users.noreply.github.com>
2023-09-11 17:42:26 +0530

    Add files via upload

---
+++ books/java.js
@@ -0,0 +1,6 @@
+let forensicbut=document.querySelector('.forensic')
+let forensicbooks=document.querySelector('.forensicbooks')
+forensicbut.onclick=function(){
+    console.log("hello")
+    forensicbooks.classList.toggle('active')
+}
---
+++ java.js
@@ -0,0 +1,7 @@
+console.log("hellow rold")
+
+const pyqbutton=document.getElementById("pyq")
+
+//pyqbutton.addEventListener('click',function(){
+  //  pyqbutton.style.color="green";
+//})
